7681f7b3fed6fe1ea50a219ebe1175367b7873f1
YujinnyPark <yujinny.park@samsung.com>
2019-09-20 11:18:19 +0900

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "2019.09.20 - Hello World"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
